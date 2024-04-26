11bea9b897adcff3cae6e520f98dad69b7bc626d
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-04-26 18:51:20 +0530

    hey

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 
 import './App.css'
-
+import { Button } from "@/components/ui/button"
 function App() {
 
 
   return (
     <>
      <div className=' text-xl font-bold text-blue-500'>Food Cosmosis</div>
+     <Button>Hi</Button>
     </>
   )
 }
