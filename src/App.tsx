a56ba0de6b8236162424bf3568dbc90fc6ee1f39
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-04-26 19:03:01 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { Button } from "@/components/ui/button"
+import { Button } from './components/ui/button'
 function App() {
 
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@shadcn/ui': './node_modules/@shadcn/ui',
+      "@": path.resolve(__dirname, "./src"),
     },
   },
+  
 })
