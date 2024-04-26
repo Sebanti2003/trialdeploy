6a8f6b7fe6f1ca13d0c9b768f2fb9dc125c0d8d7
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-04-26 19:05:28 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
      <div className=' text-xl font-bold text-blue-500'>Food Cosmosis</div>
-     <Button className='bg-red-500 text-white'>Hi</Button>
+     <Button className='bg-red-500 text-white p-5'>Hi</Button>
     </>
   )
 }
