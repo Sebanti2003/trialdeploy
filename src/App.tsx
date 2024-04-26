032eaf331b12d108c5fa785a27bfcfc374b5f0dc
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-04-26 19:09:20 +0530

    DONE

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 import './App.css'
 import { Button } from './components/ui/button'
 function App() {
-
+  const alertfunction=()=>{
+    alert(" Hello This Is Me Sebanti Dasgupta")
+  }
 
   return (
     <>
      <div className=' text-xl font-bold text-blue-500'>Food Cosmosis</div>
-     <Button className='bg-red-500 text-white p-5 m-5'>Hi</Button>
+     <Button onClick={alertfunction} className='bg-red-500 text-white p-5 m-5'>Hi</Button>
     </>
   )
 }
